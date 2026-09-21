e1508e17a5fd8f5a2892586791a1fa466269b2e3
Yaroslav U <yaroslavthedev@gmail.com>
2026-09-21 12:17:18 +0200

    feat(frontend): define match types

---
+++ frontend/src/types.ts
@@ -0,0 +1,26 @@
+export type Role = 'seaker' | 'hider';
+export type GameStatus = 'waiting' | 'running' | 'finished';
+
+export interface Position {
+	x: number,
+	y: number,
+}
+
+export interface PlayerInfo {
+	sockedId: string,
+	position: Position
+}
+
+export interface MatchState {
+	roomId: string;
+	status: GameStatus;
+	players: {
+		seeker: PlayerInfo | null;
+		hider: PlayerInfo | null;
+	};
+	timeRemaining: number;
+	winner: Role | null;
+}
+
+export const GRID_SIZE = 10;
+export const GAME_LENGTH_SECONDS = 60;
