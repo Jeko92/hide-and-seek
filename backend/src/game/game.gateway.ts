b74f660471d9c6e51d41313590eecef207586942
Yaroslav U <yaroslavthedev@gmail.com>
2026-09-21 12:57:33 +0200

    feat(gateway): add minimal implementation matchState broadcast

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,44 +1,46 @@
 import {
-  ConnectedSocket,
-  MessageBody,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
+	ConnectedSocket,
+	MessageBody,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { GameService } from './game.service';
 
 @WebSocketGateway({
-  cors: {
-    origin: 'http://localhost:5173',
-  },
+	cors: {
+		origin: 'http://localhost:5173',
+	},
 })
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  @WebSocketServer()
-  server!: Server;
+	@WebSocketServer()
+	server!: Server;
 
-  constructor(private readonly gameService: GameService) {}
+	constructor(private readonly gameService: GameService) { }
 
-  async handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
-    const assignment = this.gameService.assignToRoom(client.id);
-    await client.join(assignment.roomId);
-    client.emit('role', { role: assignment.role });
-    console.log('assigned to room', assignment);
-  }
+	async handleConnection(client: Socket) {
+		console.log(`Client connected: ${client.id}`);
+		const assignment = this.gameService.assignToRoom(client.id);
+		await client.join(assignment.roomId);
+		console.log(this.gameService.getMatch(assignment.roomId));
+		client.emit('role', { role: assignment.role });
+		console.log('assigned to room', assignment);
+		this.server.to(assignment.roomId).emit('matchState', { placeholder: true });
+	}
 
-  handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
-  }
+	handleDisconnect(client: Socket) {
+		console.log(`Client disconnected: ${client.id}`);
+	}
 
-  @SubscribeMessage('ping')
-  handlePing(
-    @MessageBody() payload: unknown,
-    @ConnectedSocket() client: Socket,
-  ) {
-    console.log('received ping:', payload);
-    client.emit('pong', { receivedAt: Date.now() });
-  }
+	@SubscribeMessage('ping')
+	handlePing(
+		@MessageBody() payload: unknown,
+		@ConnectedSocket() client: Socket,
+	) {
+		console.log('received ping:', payload);
+		client.emit('pong', { receivedAt: Date.now() });
+	}
 }
